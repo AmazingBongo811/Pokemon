28110f3f2d96b6525d6da27708080e8ce7ac98bb
AmazingBongo811 <aron.lakatos123@gmail.com>
2022-09-13 20:52:29 +0200

    Update main.java

--- lib/src/main/java/pokeapp/main.java
+++ lib/src/main/java/pokeapp/main.java
@@ -3,8 +3,8 @@ package pokeapp;
 public class main {
 
 	public static void main(String[] args) {
-		Poke_Home_Page home = new Poke_Home_Page();
-		home.Select_Screen();
+		PokeHomePage home = new PokeHomePage();
+		home.selectScreen();
 
 	}
 
